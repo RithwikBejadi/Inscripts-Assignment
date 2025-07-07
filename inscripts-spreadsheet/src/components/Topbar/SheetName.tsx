9fdf5fad935cb25d74eb41e1cdfd1698d3aab2c3
Sai Rithwik Bejadi <rithwikbejadi696@gmail.com>
2025-07-07 14:40:15 +0530

    redid it

--- inscripts-spreadsheet/src/components/Topbar/SheetName.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SheetName: React.FC = () => {
-  return (
-    <div className="text-base font-semibold text-gray-800">Spreadsheet 3</div>
-  );
-};
-
-export default SheetName; 
